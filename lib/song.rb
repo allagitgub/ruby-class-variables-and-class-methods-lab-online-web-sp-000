98d35ed1b60e14be75525adfedc2899c3ed1574e
Alla Kuzminova <alchik@gmail.com>
2020-10-12 00:51:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ attr_accessor :name, :artist, :genre
   @@artist_count = {}
 
   def initialize(name, artist, genre)
-    count =+ 1
+    @@count =+ 1
     @name = name
     @artist = artist
     @genre = genre
